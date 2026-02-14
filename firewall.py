27cfba568b5d7b5b0b84f564b459407639079391
Xirci_ylc <abdullahduranyalcin@gmail.com>
2026-02-14 20:40:09 +0300

    Delete firewall.py

--- firewall.py
+++
@@ -1,9 +0,0 @@
-import subprocess
-
-def block_ip(ip):
-    command = (
-        f'netsh advfirewall firewall add rule '
-        f'name="BLOCK_{ip}" dir=in action=block remoteip={ip}'
-    )
-    subprocess.run(command, shell=True)
-    print(f"🔥 IP BLOCKLANDI: {ip}")
